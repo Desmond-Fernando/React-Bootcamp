d71f85d1e135a256c1c7c202d6462b6d40d3cb51
Desmond-Fernando <desmond.vinod@gmail.com>
2024-04-11 09:33:53 +0100

    ReactJs intro

---
+++ KeyConcepts/intro.js
@@ -0,0 +1,56 @@
+// Declarative vs Imperative
+
+// Imparative -  Directly changes the DOM from the code
+// Declarative - Give states and React will take care of it
+
+// React Cpncepts
+// 1. Dont touch the DOM. I'll do it
+// 2. Building websites like lego blocks
+// 3. Unidirectional Data flow
+// 4. UI the resi is up to you
+
+// Resources -
+// material-ui.com
+// react-bootstrap
+// blueprintjs
+
+// https://reactdesktop.js.org/demo/
+// https://github.com/facebookarchive/react-360
+// https://github.com/Yomguithereal/react-blessed
+
+// Declarative
+// JSX
+// State
+// Props
+// VirtualDOM
+// Components
+
+// The job of a good react developer
+// Decide on Components
+// Decide the State and where it lives
+// What changes when state changes
+
+// https://github.com/nvm-sh/nvm
+
+// node -v
+// npm -v
+// yarn -v
+// pnpm - v
+
+/*
+NPM vs YARN
+Throughout the course we will be using Yarn, however, using NPM or Yarn is a personal choice. You can use this as a reference guide for the two different commands you can use. Don't worry if you are not sure of the below commands as we will explore them throughout the course:
+
+Install dependencies from package.json: npm install == yarn
+
+Install a package and add to package.json: npm install package --save == yarn add package
+
+Install a devDependency to package.json: npm install package --save-dev == yarn add package --dev
+
+Remove a dependency from package.json: npm uninstall package --save == yarn remove package
+
+Upgrade a package to its latest version: npm update --save == yarn upgrade
+
+Install a package globally: npm install package -g == yarn global add package
+
+*/
