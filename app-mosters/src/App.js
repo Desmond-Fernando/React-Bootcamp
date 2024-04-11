64b007ee71fe53a4e4c060659221f0ddbbf06d62
Desmond-Fernando <desmond.vinod@gmail.com>
2024-04-11 17:18:22 +0100

    Building Search functionality

--- app-mosters/src/App.js
+++ app-mosters/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
     console.log('1 - constructor');
   }
@@ -35,13 +36,37 @@ class App extends Component {
       });
   }
 
+  // Exrtacting anonymous function to defined method
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   // React will rerender this when ever state changes
   // this is the 2nd method to run
   render() {
     console.log('2 - render');
+
+    // Using destructring instead of using 'this' everywhere
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             // This key is nessary otuer element so that React to effeciently decide which only or needed component to render rather than everything
             <div key={monster.id}>
